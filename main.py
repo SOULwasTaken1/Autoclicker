c3229dd5397b03c9550e0d93be72b2bb83224d9e
VOID SOUL <80249387+SOULwasTaken1@users.noreply.github.com>
2022-12-20 17:23:16 +0600

    Update main.py
    added comments

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-"""
-https://github.com/SOULwasTaken1
-Made by SOULisCOOL1
-discord - SOUL#7093
-"""
-
+#import Panel
 import sys
 from tkinter import *
 import time
@@ -13,7 +8,6 @@ from pynput.mouse import Button, Controller
 import customtkinter
 #=======================================
 customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("dark-blue")
 #=======================================
 
 root = customtkinter.CTk()
@@ -21,39 +15,39 @@ root.geometry("500x300")
 root.title("AutoClicker")
 try:
 
-    root.iconbitmap("autoclicker.ico")
+    root.iconbitmap("autoclicker.ico") #bitmap icon for the GUI
 except:
-    print("image not found")
+    print("image not found") #have the bitmap image as .ico in the same folder as the script
 
-frame2 = customtkinter.CTkFrame(master=root)
-frame2.pack(pady=5,padx=10,fill="both",expand=True)
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=5,padx=10,fill="both",expand=True)
 
 
-label=customtkinter.CTkLabel(frame2,text='\t\t𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦',font=('roboto',25))
+label=customtkinter.CTkLabel(frame,text='\t\t𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦',font=('roboto',25)) #settings title 
 label.grid(row=1,)
 
-l1=customtkinter.CTkLabel(frame2,text="𝗧𝗢𝗚𝗚𝗟𝗘 𝗞𝗘𝗬",font=('roboto',20))
+l1=customtkinter.CTkLabel(frame,text="𝗧𝗢𝗚𝗚𝗟𝗘 𝗞𝗘𝗬",font=('roboto',20)) #label for the toggle button
 l1.grid(row=2,column=0)
 
-e1=customtkinter.CTkButton(frame2,text="F6")
+e1=customtkinter.CTkButton(frame,text="F6") #toggle button (doesnt work)
 e1.grid(row=2,column=1,padx=(0,100),pady=10)
 
-l2=customtkinter.CTkLabel(frame2,text="𝗖𝗟𝗢𝗦𝗘",font=('roboto',20))
+l2=customtkinter.CTkLabel(frame,text="𝗖𝗟𝗢𝗦𝗘",font=('roboto',20)) #label for the exit button
 l2.grid(row=3,column=0)
 
-e2=customtkinter.CTkButton(frame2,text="NumLock")
+e2=customtkinter.CTkButton(frame,text="NumLock") #exit button (doesnt work)
 e2.grid(row=3,column=1,padx=(0,100),pady=10)
 
-l3=customtkinter.CTkLabel(frame2,text="𝗗𝗘𝗟𝗔𝗬",font=('roboto',20))
+l3=customtkinter.CTkLabel(frame,text="𝗗𝗘𝗟𝗔𝗬",font=('roboto',20)) #label for delay Entry
 l3.grid(row=4,column=0)
 
-entry=customtkinter.CTkEntry(frame2,placeholder_text="0.001")
+entry=customtkinter.CTkEntry(frame,placeholder_text="0.001") #delay (in seconds)
 entry.grid(row=4,column=1,padx=(0,100),pady=10)
 
-l4=customtkinter.CTkLabel(frame2,text="𝗠𝗢𝗨𝗦𝗘 𝗕𝗨𝗧𝗧𝗢𝗡",font=('roboto',20))
+l4=customtkinter.CTkLabel(frame,text="𝗠𝗢𝗨𝗦𝗘 𝗕𝗨𝗧𝗧𝗢𝗡",font=('roboto',20)) #label for the click mode changer
 l4.grid(row=5,column=0)
 
-Option = customtkinter.CTkComboBox(frame2,values=["Left Click","Right Click"])
+Option = customtkinter.CTkComboBox(frame,values=["Left Click","Right Click"])  #click mode changer
 Option.grid(row=5,column=1,padx=(0,100),pady=10)
 
 
@@ -82,9 +76,9 @@ def on_press(key):
 
 def start():
     try:
-        DELAY = int(entry.get())
+        DELAY = int(entry.get()) #gets the amount of delay (in sec) you put in the GUI
     except:
-        DELAY = 0.0001
+        DELAY = 0.0001 #default delay if you dont put any amount 
     button = Option.get()
     global MOUSE_BUTTON
     if button == "Left Click":
@@ -103,7 +97,7 @@ def start():
             time.sleep(DELAY)
     listener.stop()
 
-button = customtkinter.CTkButton(root,text="START",command=start)
+button = customtkinter.CTkButton(root,text="START",command=start) #start button
 button.pack(pady=12,padx=(0,10))
 
 root.mainloop()
